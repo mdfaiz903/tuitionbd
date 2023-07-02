bcda1c892b0f214ee049eec745b0275387949fc9
fayezmohammad096 <fayezmohammad096@gmail.com>
2023-07-02 15:39:52 +0600

    create contact form using bootstrap form data submission model creat superuser creat admin.py register file creat

--- config/tuition/urls.py
+++ config/tuition/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from . views import homeview
+from . views import homeview,contact
 
 urlpatterns = [
     
     path('home/', homeview, name= "home"),
+    path('contact/', contact, name= "contact"),
 ]
--- config/tuition/views.py
+++ config/tuition/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def homeview(request):
     return render(request,'home.html')
+
+
+def contact(request):
+    if request.method=='POST':
+        name = request.POST['name']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        data = Contact(name=name,phone=phone,content=content)
+        data.save()
+    
+    return render(request,'contact.html')
